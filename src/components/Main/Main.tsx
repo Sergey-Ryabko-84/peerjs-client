43468f25914038babccd67ec5924969aa1de2f08
serhii.ryabko <serhii.ryabko@gmail.com>
2023-10-17 18:26:49 +0300

    add socket

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import { socket } from "../../socket";
+
 export const Main = () => {
+  const [status, setStatus] = useState("No connection");
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      setStatus("Connected to the server!");
+      console.log("Connected to the server!");
+    });
+
+    console.log("socket", socket);
+
+    return () => {
+      socket.off("connect");
+    };
+  }, []);
+
   return (
     <div>
       <h1>MAIN PAGE</h1>
+      <h2>{status}</h2>
     </div>
   );
 };
