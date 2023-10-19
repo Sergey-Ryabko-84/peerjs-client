273210a1af8419c1da902151e865476cd9cc809c
serhii.ryabko <serhii.ryabko@gmail.com>
2023-10-19 17:16:26 +0300

    add Room

---
+++ src/components/VideoCard/VideoCard.tsx
@@ -0,0 +1,12 @@
+import { useRef } from "react";
+
+export const VideoCard = (props: any) => {
+  const ref = useRef();
+  const peer = props.peer;
+
+  return (
+    <div>
+      <h1>VideoCard</h1>
+    </div>
+  );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
 export { Main } from "./Main";
 export { Room } from "./Room";
 export { NotFound } from "./NotFound";
+export { VideoCard } from "./VideoCard";
+export { BottomBar } from "./BottomBar";
+export { Chat } from "./Chat";
