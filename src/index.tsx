be91912525895ceaa2a5e07bf8e1c48c3d24f295
serhii.ryabko <serhii.ryabko@gmail.com>
2023-10-24 18:51:57 +0300

    add new app

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "normalize.css";
-import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+import { RoomProvider } from "./context/RoomContext";
+import { Home } from "./pages/Home";
+import { Room } from "./pages/Room";
+import { UserProvider } from "./context/UserContext";
+import { ChatProvider } from "./context/ChatContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <UserProvider>
+        <RoomProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/room/:id"
+              element={
+                <ChatProvider>
+                  <Room />
+                </ChatProvider>
+              }
+            />
+          </Routes>
+        </RoomProvider>
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
