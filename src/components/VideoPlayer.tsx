b60c167275371e87ccf028dab64ef769ce1afc53
serhii.ryabko <serhii.ryabko@gmail.com>
2023-10-26 14:17:45 +0300

    add VideoPlayer container

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@mui/material";
 import { useEffect, useRef } from "react";
 
 export const VideoPlayer: React.FC<{ stream?: MediaStream; muted?: boolean }> = ({
@@ -10,12 +11,16 @@ export const VideoPlayer: React.FC<{ stream?: MediaStream; muted?: boolean }> = 
     if (videoRef.current && stream) videoRef.current.srcObject = stream;
   }, [stream]);
   return (
-    <video
-      data-testid="peer-video"
-      style={{ width: "100%" }}
-      ref={videoRef}
-      autoPlay
-      muted={muted}
-    />
+    <Box sx={{ position: "relative", width: "100%", paddingTop: "75%" }}>
+      <Box sx={{ position: "absolute", top: 0, bottom: 0, right: 0, left: 0 }}>
+        <video
+          data-testid="peer-video"
+          style={{ width: "100%" }}
+          ref={videoRef}
+          autoPlay
+          muted={muted}
+        />
+      </Box>
+    </Box>
   );
 };
