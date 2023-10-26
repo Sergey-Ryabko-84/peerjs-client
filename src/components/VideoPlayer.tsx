51d1197a1b29645df95a7af7fe16c05251db5487
serhii.ryabko <serhii.ryabko@gmail.com>
2023-10-26 14:30:59 +0300

    changed video to CardMedia in VideoPlayer

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,5 +1,5 @@
-import { Box } from "@mui/material";
 import { useEffect, useRef } from "react";
+import { Box, CardMedia } from "@mui/material";
 
 export const VideoPlayer: React.FC<{ stream?: MediaStream; muted?: boolean }> = ({
   stream,
@@ -13,12 +13,12 @@ export const VideoPlayer: React.FC<{ stream?: MediaStream; muted?: boolean }> = 
   return (
     <Box sx={{ position: "relative", width: "100%", paddingTop: "75%" }}>
       <Box sx={{ position: "absolute", top: 0, bottom: 0, right: 0, left: 0 }}>
-        <video
-          data-testid="peer-video"
-          style={{ width: "100%" }}
+        <CardMedia
+          component="video"
           ref={videoRef}
           autoPlay
           muted={muted}
+          // style={{ width: "100%" }}
         />
       </Box>
     </Box>
